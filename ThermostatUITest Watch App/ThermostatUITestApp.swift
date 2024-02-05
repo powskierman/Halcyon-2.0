c0c546d62ff87f02ac5eda3d749948a7145a94bb
powskierman <mlapoint99@hotmail.com>
2024-02-04 15:22:58 -1000

    Initial Commit

---
+++ ThermostatUITest Watch App/ThermostatUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermostatUITestApp.swift
+//  ThermostatUITest Watch App
+//
+//  Created by Michel Lapointe on 2024-02-04.
+//
+
+import SwiftUI
+
+@main
+struct ThermostatUITest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
